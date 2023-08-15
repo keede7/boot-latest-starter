c65bdaed8bde2ec09fb8be30835ec16322d51c02
keede <wesaq11@gmail.com>
2023-08-15 12:49:26 +0900

    3.x Version Init

---
+++ src/main/java/io/keede/bootlateststarter/BootLatestStarterApplication.java
@@ -0,0 +1,13 @@
+package io.keede.bootlateststarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootLatestStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootLatestStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/keede/bootlateststarter/BootLatestStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.keede.bootlateststarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootLatestStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
