88e015bfde788c15deab75a93500f01a9cc2dae5
keede <wesaq11@gmail.com>
2023-08-22 00:24:40 +0900

    feat: 예제 Entity 준비

---
+++ src/main/java/io/keede/bootlateststarter/domains/user/entity/User.java
@@ -0,0 +1,30 @@
+package io.keede.bootlateststarter.domains.user.entity;
+
+import jakarta.persistence.*;
+
+/**
+* @author keede
+* Created on 2023/08/22
+*/
+@Entity
+@Table(name = "user_t")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "email", nullable = false, length = 50)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    protected User() {}
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+}
